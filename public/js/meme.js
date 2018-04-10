1c586f1c0015081c276db3da432f50888e02afc8
Robert Herley <rherley@stevens.edu>
2018-04-10 02:55:12 -0400

    ⏰

--- public/js/meme.js
+++ public/js/meme.js
@@ -1,3 +1,19 @@
+class TidePod {
+   constructor() {
+      this._elem = document.createElement('div');
+      this._elem.className = 'tidepod';
+      this._elem.innerHTML = 'howdy';
+      document.body.appendChild(this._elem);
+   }
+
+   move({ x, y }) {
+      console.log(this);
+      this._elem.style.top = `${y}px`;
+      this._elem.style.left = `${x}px`;
+   }
+}
+
 window.onload = () => {
-   console.log('it works!');
+   const pod = new TidePod();
+   document.body.addEventListener('mousemove', e => pod.move(e));
 };
